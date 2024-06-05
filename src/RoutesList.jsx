aa34893ce9073f247e535e853671b9211efe4f84
Anya Aven <leafeon96@gmail.com>
2024-06-05 14:00:06 -0400

    Finished jobs and companies routes

--- src/RoutesList.jsx
+++ src/RoutesList.jsx
@@ -1,12 +1,17 @@
+import { Routes, Route } from "react-router";
+import JobList from "./JobList.jsx";
+import CompanyList from "./CompanyList.jsx";
+import CompanyDetail from "./CompanyDetail.jsx";
+import Homepage from "./Homepage.jsx";
 
 /**
  * RoutesList
  *
- * Props:
+ * Props: none
  *
- * State:
+ * State: none
  *
- * Effects:
+ * Effects: none
  */
 
 function RoutesList() {
@@ -14,7 +19,28 @@ function RoutesList() {
 
   return (
     <div className="RoutesList">
-
+      <Routes>
+      <Route
+          path="/"
+          element={<Homepage />}
+        />
+        <Route
+          path="/jobs"
+          element={<JobList />}
+        />
+        <Route
+          path="/companies"
+          element={<CompanyList />}
+        />
+        <Route
+          path="/companies/:name"
+          element={<CompanyDetail />}
+        />
+        <Route
+          path="*"
+          element={<Homepage />}
+        />
+      </Routes>
     </div>
   );
 }
