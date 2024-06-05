94223d7403c7e60cabc241d91a391e62105a6a7b
Anya Aven <leafeon96@gmail.com>
2024-06-05 16:36:48 -0400

    Added a mock for router

--- src/Navigation.test.jsx
+++ src/Navigation.test.jsx
@@ -1,14 +1,18 @@
 import { describe, expect, test, vi, beforeAll, afterEach, afterAll } from "vitest";
 
 import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
 import Navigation from "./Navigation.jsx";
 
 
-//FIXME: need to mock the router
 describe("smoke tests", function () {
 
   test('renders', function () {
-    // render(<Navigation />);
+    render(
+      <MemoryRouter>
+        <Navigation />
+      </MemoryRouter>
+    );
   });
 
 });
--- src/RoutesList.test.jsx
+++ src/RoutesList.test.jsx
@@ -1,13 +1,18 @@
 import { describe, expect, test, vi, beforeAll, afterEach, afterAll } from "vitest";
 
 import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
 import RoutesList from "./RoutesList.jsx";
 
-//FIXME: need to mock the router
+
 describe("smoke tests", function () {
 
   test('renders', function () {
-    // render(<RoutesList />);
+    render(
+      <MemoryRouter>
+        <RoutesList />
+      </MemoryRouter>
+    );
   });
 
 });
