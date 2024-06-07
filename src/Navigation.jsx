2a103e74efa80098d163cb10dd6c66eb90590fdd
Anya Aven <leafeon96@gmail.com>
2024-06-07 13:53:03 -0400

    Added logged in and logged out routes WIP

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,5 +1,7 @@
 import { NavLink } from "react-router-dom";
+import { useContext } from "react";
 import "./Navigation.css";
+import userContext from "./userContext.js";
 
 /**
  * Navigation
@@ -13,20 +15,50 @@ import "./Navigation.css";
  *  App -> Navigation
  */
 
-function Navigation() {
-  console.log("Navigation");
+function Navigation({ logout }) {
+
+  const { firstName, username } = useContext(userContext);
+  console.log("Navigation", {firstName, username});
 
   return (
     <nav className="Navigation">
       <NavLink to="" end>
         Jobly
       </NavLink>
-      <NavLink to="/companies" end>
-        Companies
-      </NavLink>
-      <NavLink to="/jobs" end>
-        Jobs
-      </NavLink>
+
+      {/* LOGGED IN LINKS */}
+      {
+        firstName !== null && username !== null &&
+        <>
+          <NavLink to="/companies" end>
+            Companies
+          </NavLink>
+          <NavLink to="/jobs" end>
+            Jobs
+          </NavLink>
+          <NavLink to="/profile" end>
+            Profile
+          </NavLink>
+          <NavLink onClick={() => logout()} end>
+            Log out {username}
+          </NavLink>
+        </>
+      }
+
+      {/* LOGGED OUT ONLY LINKS */}
+      {
+        firstName === null && username === null &&
+        <>
+          <NavLink to="/signup" end>
+            Sign Up
+          </NavLink>
+          <NavLink to="/login" end>
+            Login
+          </NavLink>
+        </>
+      }
+
+
     </nav>
   );
 }
