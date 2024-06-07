7b72ba7f4b7931ffadff5fdd005de81ce1373795
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-06-06 17:13:40 -0700

    updated user login form

--- src/UserLoginForm.jsx
+++ src/UserLoginForm.jsx
@@ -4,20 +4,20 @@ import Alert from "./Alert.jsx";
 /**
  * UserLoginForm
  *
- * Props: { alerts: [alert, ...], handleSave }
+ * Props: { errors: [error, ...], handleUserLogin }
  *
  * State: formData
  *
  * RoutesList -> UserLogin -> UserLoginForm
  */
 
-function UserLoginForm({ alerts = [], handleSave }) {
+function UserLoginForm({ errors = [], handleUserLogin }) {
   console.log("UserLoginForm");
 
   const [formData, setFormData] = useState({
     username: "",
     password: "",
-    alerts
+    errors
   });
 
 
@@ -39,23 +39,27 @@ function UserLoginForm({ alerts = [], handleSave }) {
   }
 
   return (
-    <form className="UserLoginForm" onSubmit={handleSubmit}>
-      <label htmlFor="UserLoginForm-username">Username</label>
-      <input
-        id="UserLoginForm-username"
-        value={formData.username}
-        onChange={handleChange}
-      />
-      <label htmlFor="UserLoginForm-password">Password</label>
-      <input
-        id="UserLoginForm-password"
-        value={formData.password}
-        type="password"
-        onChange={handleChange}
-      />
-      <Alert alerts={formData.alerts}/>
-      <button>Login</button>
-    </form>
+    <div className="UserLoginForm">
+      <h1>Login</h1>
+      <form className="UserLoginForm-form" onSubmit={handleSubmit}>
+        <label htmlFor="UserLoginForm-username">Username</label>
+        <input
+          id="UserLoginForm-username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+        <label htmlFor="UserLoginForm-password">Password</label>
+        <input
+          id="UserLoginForm-password"
+          value={formData.password}
+          type="password"
+          onChange={handleChange}
+        />
+        <Alert alerts={formData.alerts} />
+        <button>Login</button>
+      </form>
+    </div>
+
   );
 }
 
