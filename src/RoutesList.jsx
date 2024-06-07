477cdf9b8bf6df17b850926f6c0ef3dbb9465757
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-06-07 11:26:37 -0700

    finished routes list

--- src/RoutesList.jsx
+++ src/RoutesList.jsx
@@ -3,6 +3,11 @@ import JobList from "./JobList.jsx";
 import CompanyList from "./CompanyList.jsx";
 import CompanyDetail from "./CompanyDetail.jsx";
 import Homepage from "./Homepage.jsx";
+import UserLoginForm from "./UserLoginForm.jsx";
+import UserRegistrationForm from "./UserRegistrationForm.jsx";
+import UserProfileForm from "./UserProfileForm.jsx";
+import { useContext } from "react";
+import userContext from "./userContext.js";
 
 /**
  * RoutesList
@@ -19,27 +24,55 @@ import Homepage from "./Homepage.jsx";
 function RoutesList() {
   console.log("RoutesList");
 
+  const { token } = useContext(userContext);
+
   return (
     <div className="RoutesList">
+
       <Routes>
         <Route
           path="/"
           element={<Homepage />}
         />
+
+        {/* LOGGED IN ROUTES */}
+        {token &&
+          <>
+            <Route
+              path="/jobs"
+              element={<JobList />}
+            />
+            <Route
+              path="/companies"
+              element={<CompanyList />}
+            />
+            <Route
+              path="/companies/:handle"
+              element={<CompanyDetail />}
+            />
+            <Route
+              path="/profile"
+              element={<UserProfileForm />}
+            />
+          </>
+        }
+
+        {/* LOGGED OUT ROUTES */}
+        {!token &&
+          <>
+            <Route
+              path="/login"
+              element={<UserLoginForm />}
+            />
+            <Route
+              path="/signup"
+              element={<UserRegistrationForm />}
+            />
+          </>
+        }
+
         <Route
-          path="/jobs"
-          element={<JobList />}
-        />
-        <Route
-          path="/companies"
-          element={<CompanyList />}
-        />
-        <Route
-          path="/companies/:handle"
-          element={<CompanyDetail />}
-        />
-        <Route
-          path="/*"
+          path="*"
           element={<Navigate to="/" />}
         />
       </Routes>
